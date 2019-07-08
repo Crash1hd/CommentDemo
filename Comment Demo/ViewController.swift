4e8a44bf0c61fd6241448d40bd4960559efd55f9
Adam Kirk <A.kirk@lycosidea.com>
2019-07-07 21:18:39 -0600

    Initial Comment Demo Application Commit

---
+++ Comment Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comment Demo
+//
+//  Created by Adam Kirk on 2019-07-07.
+//  Copyright © 2019 Lycosidea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
