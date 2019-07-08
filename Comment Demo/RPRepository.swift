3a114937c4f76ae656c5df8b8a7eacba62afa5b6
Adam Kirk <A.kirk@lycosidea.com>
2019-07-07 21:31:37 -0600

    Adding in RPRepo initial files

---
+++ Comment Demo/RPRepository.swift
@@ -0,0 +1,15 @@
+//
+//  RPRepository.swift
+//  Comment Demo
+//
+//  Created by Adam Kirk on 2019-07-07.
+//  Copyright © 2019 Lycosidea. All rights reserved.
+//
+
+import Foundation
+
+class RPRepository {
+
+	init() {}
+
+}
---
+++ Comment DemoTests/RPRepositoryTests.swift
@@ -0,0 +1,28 @@
+//
+//  RPRepository.swift
+//  Comment DemoTests
+//
+//  Created by Adam Kirk on 2019-07-07.
+//  Copyright © 2019 Lycosidea. All rights reserved.
+//
+
+import XCTest
+@testable import Comment_Demo
+
+class RPRepositoryTests: XCTestCase {
+
+	override func setUp() {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+		super.setUp()
+	}
+
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+
+	func testDoesRepoExist() {
+		XCTAssertNotNil(RPRepository())
+	}
+
+}
